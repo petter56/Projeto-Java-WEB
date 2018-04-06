09b4bde742d888072f2bf3ad5e04bf069034b1a2
Brain <Brain@LAPTOP-QJBK2S0M>
2018-04-06 16:21:35 -0300

    consegui

---
+++ projetoPDP/src/model/Usuario.java
@@ -0,0 +1,37 @@
+package model;
+//comentario
+public class Usuario {
+	
+	private String cfp;
+	private String nome;
+	private String placa;
+	private String senha;
+	
+	public String getCfp() {
+		return cfp;
+	}
+	public void setCfp(String cfp) {
+		this.cfp = cfp;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+
+}
